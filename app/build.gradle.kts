a21eb2646daf38bd9a18a546162911820a023f19
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-16 14:57:19 -0600

    chore: bump app version to 13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "12.0"
+    versionCode = 13
+    versionName = "13.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
