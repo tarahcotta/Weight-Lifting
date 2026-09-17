485a1155b1d77c5236fa4e95ae52f232c16c86aa
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-16 22:27:07 -0600

    build: bump version to 14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity"
     minSdk = 24
     targetSdk = 36
-    versionCode = 13
-    versionName = "13.0"
+    versionCode = 14
+    versionName = "14.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
